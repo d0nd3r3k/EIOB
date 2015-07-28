ae64c7649363d05c538b60baf945da6ebed13c18
Donald Derek <donaldderek.haddad@gmail.com>
2015-07-29 00:33:44 +0300

    int socketio

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var staticRoot = path.join(__dirname+'/public/');
 
 //Expose static content
@@ -11,8 +13,15 @@ app.get('/mf', function(req, res){
     res.sendFile(staticRoot+"index.html");
 });
 
-//Server
-var server = app.listen(3000, function(){
+//WS Server
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+server.listen(3000,function(){
     var port = server.address().port;
     var host = server.address().address;
     (host == '::') ? host = 'localhost' : host = host;
